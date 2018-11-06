0a154b69a8cfb7b46700e9cdd13476341ff4edfb
이준영 <lee-jy@partner.sk.com>
2018-11-06 17:53:00 +0900

    람다....?

--- src/test/java/com/lyae/LambdaTest.java
+++ src/test/java/com/lyae/LambdaTest.java
@@ -12,10 +12,18 @@ import org.apache.commons.io.FileUtils;
 
 public class LambdaTest {
 
+	 public static interface Abc {
+		  public int calc(int a, int b);
+	 }
 	
 	@FunctionalInterface
 	interface LamdaIF{
 		public void run(Map map);
+	}
+	
+	@FunctionalInterface
+	interface Func{
+		public int run(int a , int b);
 	}
 	
 	public static void main(String[] args) {
@@ -27,17 +35,21 @@ public class LambdaTest {
 			System.out.println("1"); 
 			});
 		
+		calc( 6, 8, (a, b) -> {
+			int result  = a+b;
+			System.out.println(result);
+			return result;
+		});
 		
-		
-		new File("어떤경로").listFiles((f) -> f.isDirectory());
-		//.listFiles(File::isDirectory);
-		
-		new File("어떤경로").listFiles(e -> e.getName().startsWith("ML03."));
-		
-		List<File> list = Arrays.asList(new File("경로").listFiles());
-		list.stream().filter(e -> e.isDirectory()).collect(Collectors.toList());
-		
-		
+//		new File("어떤경로").listFiles((f) -> f.isDirectory());
+//		//.listFiles(File::isDirectory);
+//		
+//		new File("어떤경로").listFiles(e -> e.getName().startsWith("ML03."));
+//		
+//		List<File> list = Arrays.asList(new File("경로").listFiles());
+//		list.stream().filter(e -> e.isDirectory()).collect(Collectors.toList());
+//		
+//		
 		
 	}
 	
@@ -53,6 +65,12 @@ public class LambdaTest {
 		return a * 34234;
 	}
 
+	public static void calc(int a, int b, Func func) {
+		System.out.println("시작");
+		func.run(a, b);
+		System.out.println("종료");
+		
+	}
 	
 	public static void test(LamdaIF lambda){
 		
