8ed4984b22232e5ae5e68da2f950e8a62a9cc0a5
lyae <lyaesley@gmail.com>
2019-04-17 22:46:51 +0900

    backgroud

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,4 +2,4 @@ echo "jar copy start"
 cp -f $(pwd)/target/*.jar /home/lyae/dev/war/.
 echo "jar copy end"
 java -version
-nohub java -jar /home/lyae/dev/war/lyae-*.jar -Denv=prod &
+nohup java -jar /home/lyae/dev/war/lyae-*.jar -Denv=prod &
