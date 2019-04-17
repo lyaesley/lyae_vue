79e798b7d837399e42e6233731dca066b4575aa2
lyae <lyaesley@gmail.com>
2019-04-17 23:09:55 +0900

    test

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,4 +1,4 @@
 echo "jar copy start" 
 cp -f $(pwd)/target/*.jar /home/lyae/dev/war/.
 echo "jar copy end"
-java -jar /home/lyae/dev/war/lyae-*.jar -Denv=prod
+nohup java -jar /home/lyae/dev/war/lyae-*.jar -Dspring.profiles.active=prod &
