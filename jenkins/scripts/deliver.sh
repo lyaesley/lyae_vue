faf294352de695a9311d98465dcad6750df0f455
lyae <lyaesley@gmail.com>
2019-04-17 22:49:58 +0900

    test

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,4 @@
 echo "jar copy start" 
 cp -f $(pwd)/target/*.jar /home/lyae/dev/war/.
 echo "jar copy end"
-java -version
-nohup java -jar /home/lyae/dev/war/lyae-*.jar -Denv=prod &
+java -jar /home/lyae/dev/war/lyae-*.jar -Denv=prod
